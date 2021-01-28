f6ca3323b0812fd3253b64c5b18916172dbe9b0d
Rob <rbhouck32@gmail.com>
2021-01-27 22:53:30 -0500

    Tests written and all but 1 test pass

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,11 +5,21 @@ module.exports = {
   get,
 };
 
+// items {
+//      name: "itemName",
+//      durability: 0-20,
+//      enhancement: 0-100
+//       }
 function success(item) {
+  item.enhancement < 20 ? (item.enhancement += 1) : "";
   return { ...item };
 }
 
 function fail(item) {
+  item.enhancement <= 15 ? (item.durability -= 5) : (item.durability -= 10);
+  item.enhancement >= 15
+    ? (item.durability -= 10) && (item.enhancement -= 1)
+    : "";
   return { ...item };
 }
 
